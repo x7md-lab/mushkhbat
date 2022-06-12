8a6a5344b12e1ec3c96561b385c3f112927d33a0
x7md <it7mdx@googlemail.com>
2022-06-12 08:00:49 +0300

    post reqest

--- functions/api/get-comment/[url].js
+++ functions/api/get-comment/[url].js
@@ -1,6 +1,17 @@
 export async function onRequestGet({ params }) {
-    const res = await fetch(`https://script.google.com/macros/s/AKfycbwwaTAV5kq8KAcwljglw7H8ZE5NfuuiH965vdKECOgaIkDpxxZ0bEIk1LoyJJgzAWMypA/exec?type=get-comment&post=${params.url}`);
-    const data = await res.json();
+  fetch("https://script.google.com/macros/s/AKfycbwwaTAV5kq8KAcwljglw7H8ZE5NfuuiH965vdKECOgaIkDpxxZ0bEIk1LoyJJgzAWMypA/exec",
+    {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      post: "post", uid: 'F0CiaFfqnJV3mBLeEFZsjJM4vR43',
+      name: 'حمد بنقالي',
+      pic: 'https://lh3.googleusercontent.com/a-/AOh14GjMfljIeb62Ln_siBrslVORq4waawMh94Aaxsmp-A=s96-c',
+       commentText: 'Hello'}
+      )
+    })
     const info = JSON.stringify(data, null, 2);
     return new Response(info);
   }
