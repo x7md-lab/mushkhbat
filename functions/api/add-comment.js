c820b5f5fd71392eceb27a4f5d137c7d6d1b981a
x7md <it7mdx@googlemail.com>
2022-06-12 08:55:25 +0300

    parse req body

--- functions/api/add-comment.js
+++ functions/api/add-comment.js
@@ -1,19 +1,21 @@
-export async function onRequestPost(){
+export async function onRequestPost(request){
+    const { headers } = request;
+    const contentType = headers.get('content-type') || '';
+
+    if (contentType.includes('application/json')) {
+        let bodyData = await request.json();
         let req =  await fetch("https://script.google.com/macros/s/AKfycbwwaTAV5kq8KAcwljglw7H8ZE5NfuuiH965vdKECOgaIkDpxxZ0bEIk1LoyJJgzAWMypA/exec",
-          {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            post: "post", uid: 'F0CiaFfqnJV3mBLeEFZsjJM4vR43',
-            name: 'حمد بنقالي',
-            pic: 'https://lh3.googleusercontent.com/a-/AOh14GjMfljIeb62Ln_siBrslVORq4waawMh94Aaxsmp-A=s96-c',
-             commentText: 'Hello'}
-            )
-          })
-          let data = await req.json();
-          const info = JSON.stringify(data, null, 2);
-          return new Response(info);
+        {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+        })
+        let data = await req.json();
+        const info = JSON.stringify(data, null, 2);
+        return new Response(info);
+    } 
+          return new Response(JSON.stringify({done: false, error: "Not JSON"}));
       
 }
