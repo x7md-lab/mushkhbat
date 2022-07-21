dadeaa5a85bc1979ff749a50b7cbffe0b080a0b1
x7md <x7mdNet@gmail.com>
2022-07-21 22:14:08 +0300

    fix export

--- src/data/Navbar.js
+++ src/data/Navbar.js
@@ -1,6 +1,7 @@
-export default data = [
+const data = [
     {name: "الصفحة الرئيسية", link: "/"},
     {name: "تدوينة", link:"/post"},
     {name: "المواضيع", link:"/topics"},
     {name: "التدوينات", link: "/blogs"}
 ]
+export default data;
